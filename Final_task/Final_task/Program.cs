b946a5948b3696b9663bb62612c4ae8675955506
Alena <kosoohina@yandex.ru>
2022-12-27 19:09:31 +0300

    Реализация алгоритма

---
+++ Final_task/Final_task/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] start = new string[7] { "Story", "year", "new", "123", "res","task","a" };
+string[] end = new string[start.Length];
+int count = 0;
+for (int i = 0; i < start.Length; i++)
+    {
+        if (start[i].Length <= 3)
+        {
+            end[count] = start[i];
+            count++;
+        }
+    }
+
+for (int i = 0; i < end.Length; i++)
+    {
+        Console.Write($"{end[i]} ");
+    }
+    Console.WriteLine();
